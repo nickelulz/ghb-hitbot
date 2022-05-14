e10d72b4c5af0632d0d01772792bd09ffdb32c0d
Mufaro Machaya <sandmanmufmaf@gmail.com>
2022-05-14 13:35:07 -0500

    editing the command list

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -3,6 +3,8 @@ import { Hello } from './hello'
 import { ListHits } from "./listhits"
 import { PlaceHit } from "./placehit"
 import { RemoveHit } from "./removehit"
+import { ClaimHit } from "./claimhit"
+import { Register } from "./register"
 
-const commands: Command[] = [ Hello, ListHits, PlaceHit, RemoveHit ]
+const commands: Command[] = [ Hello, ListHits, PlaceHit, RemoveHit, ClaimHit, Register ]
 export default commands
